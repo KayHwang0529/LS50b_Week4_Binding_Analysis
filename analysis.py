75189fedc5e843c85d867cd42d079721cfd68ac8
katehsia <katehsia@gmail.com>
2026-03-31 13:27:07 -0400

    heatmaps!

--- analysis.py
+++ analysis.py
@@ -7,3 +7,22 @@ from scipy import stats
 # Load data
 df = pd.read_csv('VRC01/VRC01_kds.csv')
 
+print(df.head())
+
+# epistasis heatmap (average binding for every combination of two mutations)
+antigens = ['SF162', 'CH505TF']
+sites = [col for col in df.columns if 'site_' in col]
+n_sites = len(sites)
+
+for antigen in antigens:
+    df_antigen = df[df['antigen'] == antigen]
+
+    data_hm = np.zeros((n_sites, n_sites))
+
+    for i in range(n_sites):
+        for j in range(n_sites):
+            y_n_mutated = (df_antigen[sites[i]] != 'G') & (df_antigen[sites[j]] != 'G') # sees how many matured sites for each mutation
+            data_hm[i, j] = df_antigen[y_n_mutated]['nlog10_Kd'].mean()
+    sns.heatmap(data_hm, xticklabels=sites, yticklabels=sites)
+    plt.title(f'Average affinity for {antigen} pairwise mutation')
+    plt.show()
